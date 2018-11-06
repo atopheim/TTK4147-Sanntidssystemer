b3c3f7bbbce623f7e8704ec3ce412a75e5baa72c
student <student@Sanntid.(none)>
2018-11-06 14:04:02 +0100

    Finished miniproject

---
+++ Miniproject/Submission/Group27_miniproject_submission/Code/time.h
@@ -0,0 +1,10 @@
+
+#include <time.h>
+
+
+int clock_nanosleep(clockid_t clock_id, int flags, const struct timespec* request, struct timespec* remain);
+
+struct timespec timespec_normalized(time_t sec, long nsec);
+struct timespec timespec_sub(struct timespec lhs, struct timespec rhs);
+struct timespec timespec_add(struct timespec lhs, struct timespec rhs);
+int timespec_cmp(struct timespec lhs, struct timespec rhs);
